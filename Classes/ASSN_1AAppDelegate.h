148405f87a2042caf78ea907814ed766b5af97a2
Balde Silva <baldemar.silva@mail.utexas.edu>
2010-09-27 10:33:45 -0500

    First Commit of files.

---
+++ Classes/ASSN_1AAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  ASSN_1AAppDelegate.h
+//  ASSN_1A
+//
+//  Created by Baldemar Silva III on 9/24/10.
+//  Copyright 2010 The University of Texas at Austin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASSN_1AAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ASSN_1A
+//
+//  Created by Baldemar Silva III on 9/24/10.
+//  Copyright 2010 The University of Texas at Austin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
